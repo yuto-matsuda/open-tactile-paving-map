e247bb5fe56fb430b69595f575512fd8c6fe7f63
yuto-matsuda <yutochin.dev@gmail.com>
2025-07-08 17:47:30 +0900

    deploy

---
+++ src/utils/leaflet-icons.ts
@@ -0,0 +1,13 @@
+import L from 'leaflet'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+// @ts-ignore: _getIconUrl is a private Leaflet property not in the types
+delete (L.Icon.Default.prototype as any)._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
