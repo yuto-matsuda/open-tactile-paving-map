43fc8bae720e8f214b2464cdbe83ff90372f984a
yuto-matsuda <yutochin.dev@gmail.com>
2025-07-08 17:33:56 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/open-tactile-paving-map/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
 })
